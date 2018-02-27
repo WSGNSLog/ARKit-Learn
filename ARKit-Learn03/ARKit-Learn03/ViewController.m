46b96c7d1b56f93fc590ff347430323ded459860
wushiguang <wushiguang@jiuan.com>
2018-02-27 16:47:05 +0800

    SCNScene
    SCNScene

--- ARKit-Learn03/ARKit-Learn03/ViewController.m
+++ ARKit-Learn03/ARKit-Learn03/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <SceneKit/SceneKit.h>
 
 @interface ViewController ()
 
@@ -16,10 +17,97 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    [self setupSCNView];
 }
+- (void)setupSCNView{
+    
+    //创建游戏视图
+    SCNView *scnview = [[SCNView alloc]initWithFrame:self.view.bounds];
+    //加载游戏文件
+    NSURL *resourceURL = [[NSBundle mainBundle] URLForResource:@"yizi" withExtension:@"dae"];
+    scnview.scene = [SCNScene sceneWithURL:resourceURL options:nil error:nil];
+    
+    scnview.backgroundColor = [UIColor lightGrayColor];
+    scnview.allowsCameraControl = true;
+    [self.view addSubview:scnview];
 
+}
+/*
+ 4-SCNScene
 
+ 本节学习目标
+ 
+ 掌握SCNScene的基本概念
+ 主要能干什么事情
+ 怎么使用
+ 概念
+ 
+ 通俗的说就是游戏场景,游戏场景主要由几何模型,灯光,照相机，和其它的属性组成,另外请注意,SCNScene这个对象包含3D场景和场景中的内容。
+ 
+ 主要能干什么事情
+ 
+ 添加各种游戏元素到场景中
+ 读取场景文件
+ 将场景写入文件
+ 还有很多功能,这里暂时不讲,以为要用到后面的知识!我们慢慢来,最终吃完整个蛋糕。
+ 
+ 怎么使用
+ 
+ 我们创建一个工程演示一下
+ 
+ 第一步
+ 
+ 创建一个简单的工程,就和普通应用一样。这里就不演示了,在第一节中有讲到!
+ 
+ 第二步
+ 
+ 导入游戏框架
+ 
+ import SceneKit
+ 
+ 第三步
+ 
+ 创建游戏专用视图
+ 
+ let scnView = SCNView(frame: self.view.bounds);
+ 
+ 第四步
+ 
+ 加载游戏文件,请上网随便找一个后缀名为.dae的文件
+ 
+ scnView.scene = SCNScene(named: "my.dae")
+ 
+ 第五步
+ 
+ 将游戏专用视图添加到我们的视图中去
+ 
+ self.view.addSubview(scnView);
+ 
+ 这个时候运行一下工程试试看
+ 
+ 将场景写入文件中去
+ 
+ let urlString = NSHomeDirectory() + "/Documents/my.dae"
+ scnView.scene?.write(to: URL(fileURLWithPath: urlString), options: nil, delegate: nil, progressHandler: { (progress, error, flag) in
+ print( progress);
+ })
+ 
+ 然后到模拟器的目录下面去查看是否有保存的文件,怎么去查看保存文件的路径呢？网上的方法有很多都过时,并且不靠谱,最简单的方式如下
+ 
+ print(urlString)
+ 
+ /Users/xujie/Library/Developer/
+ CoreSimulator/Devices/
+ 7DBC6358-32AF-42EE-BAA4-6350562AFD73/
+ data/Containers/Data/Application/067F20AD-7CB8-4F70-A202-EBB9DA1306A3/Documents/my.dae
+ 
+ 在Finder->前往->前往文件夹->输入地址 即可找到
+ 
+ 真机怎么查看保存的文件?
+ 
+ xcode->window->Devices->选中真机->单击应用->选择设置图标->download Container... 下载下来即可
+ 
+ */
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
--- ARKit-Learn04/ARKit-Learn04/ViewController.m
+++ ARKit-Learn04/ARKit-Learn04/ViewController.m
@@ -19,11 +19,122 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+/*
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+5-理解游戏场景和节点的概念
+
+你必须知道的概念
+
+概念图
+
+场景
+
+简单的说，就是把人物，地图，道具等放在一个空间里,组成一个大的环境,这个大的环境就被称为场景!
+
+节点
+
+在SceneKit 节点是个抽象的概念,节是个看不见,摸不到的东西，没有几何形状,但是它有位置，以及自身坐标系。通俗的讲，在场景中创建一个添加节点后,你就可以在这个节点上放我们游戏元素了，比如人物模型，灯光，摄像机等等! 节点上可以添加节点的,每个节点都有自身坐标系。如图，我们把节点2添加到节点1上去。
+
+实战目标
+
+1.掌握如何添加节点到场景中 2.给节点绑定几何物体 3.给节点添加节点
+
+开始吧
+
+第一步.创建工程
+
+和创建普通工程没啥区别
+
+让学习成为一种习惯
+
+我们已经完成了创建工程的任务,有的人就问了,你不是要将SceneKit 游戏开发，怎么创建的是应用工程,往下看,就这么任性!
+
+第二步，添加我们的游戏框架到我们的控制器ViewController中
+
+添加游戏框架
+
+到这里,准备工作已经全部做完,开始敲代码啦！
+
+第三步:创建一个游戏引擎专属View视图(SCNView)
+
+@property(nonatomic,strong)SCNView *gameView; // 设置一个引用属性
+
+- (void)addSCNView{
+    // 1.创建一个边长为300 的视图，放在屏幕中心
+    self.gameView = [[SCNView alloc]initWithFrame:CGRectMake(0, 0, 300, 300)];
+    self.gameView.center = CGPointMake(CGRectGetMidX(self.view.bounds), CGRectGetMidY(self.view.bounds));
+    // 2.设置背景颜色为黑色
+    self.gameView.backgroundColor = [UIColor blackColor];
+    // 3.添加到父视图中去
+    [self.view addSubview:self.gameView];
 }
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self addSCNView];
+}
+
+此刻运行一下程序结果如下图
+
+运行结果
+
+第四步.添加场景
+
+-(void)createScene{
+    SCNScene *scene = [SCNScene scene];
+    self.gameView.scene = scene;
+}
+
+此时，如果你运行程序，就和上面的效果一样，没有变化，因为我们的场景中什么都没有。
+
+第五步.添加节点
+
+// 创建节点，添加到scene的根节点上
+SCNNode *node = [SCNNode node];
+[scene.rootNode addChildNode:node];
+
+提示:
+
+scene.rootNode 有些朋友很好奇，为什么Scene也有一个节点，上面说个，没有节点你没法放游戏元素上去，并且它有自身坐标系，这就是为什么scene也有一个根节点的原因。
+
+此时你运行程序也和上面结果一样,因为节点没有几何形状!
+
+第六步.给节点绑定一个几何形状的物体
+
+// 创建一个球体几何绑定到节点上去
+SCNSphere *sphere = [SCNSphere sphereWithRadius:0.5];
+node.geometry = sphere;
+
+运行一下
+
+运行结果
+
+此时我们已经完成了实战任务的1和2,继续加油!
+
+第七步.给节点添加节点
+
+// 创建子节点 给子节点添加几何形状
+SCNNode *childNode = [SCNNode node];
+// 设置节点的位置
+childNode.position = SCNVector3Make(-0.5, 0, 1);
+// 设置几何形状，我们选择立体字体
+SCNText *text = [SCNText textWithString:@"让学习成为一种习惯" extrusionDepth:0.03];
+// 设置字体颜色
+text.firstMaterial.diffuse.contents = [UIColor redColor];
+// 设置字体大小
+text.font = [UIFont systemFontOfSize:0.15];
+// 给几点绑定几何物体
+childNode.geometry = text;
+[node addChildNode:childNode];
+
+友情提示:
+
+extrusionDepth 字体的深度 firstMaterial.diffuse.contents 不知道没关系，后面会讲的,单词意思就可以理解.firstMaterial 第一个材质 diffuse 自身发散的contents内容，我们设置为red 也可以设置图片的!
+
+我们用一键大招让你自由旋转物体
+
+self.gameView.allowsCameraControl = true;
+
+*/
 
 @end
