d5bc8d585aec3455bb34f8fc8d408b04dd9bff95
wushiguang <wushiguang@jiuan.com>
2018-02-27 15:11:46 +0800

    注释
    添加注释

--- ARKit-Learn01/ARKit-Learn01/ViewController.m
+++ ARKit-Learn01/ARKit-Learn01/ViewController.m
@@ -22,15 +22,20 @@
 }
 - (void)setupScnview{
     
+    //创建游戏专用视图
     SCNView *scnview = [[SCNView alloc]initWithFrame:self.view.bounds];
+    //创建一个场景，系统默认是没有的
     scnview.scene = [SCNScene scene];
-    
+    //先设置一个颜色看看游戏引擎有没有加载
     scnview.backgroundColor = [UIColor lightGrayColor];
     
+    //创建一个文字节点
     SCNNode *textNode = [SCNNode node];
     SCNText *text = [SCNText textWithString:@"hello world" extrusionDepth:0.5];
     textNode.geometry = text;
+    //把这个文字节点添加到游戏场景的根结点上
     [scnview.scene.rootNode addChildNode:textNode];
+    //允许用户操作摄像机
     scnview.allowsCameraControl = true;
     [self.view addSubview:scnview];
 }
