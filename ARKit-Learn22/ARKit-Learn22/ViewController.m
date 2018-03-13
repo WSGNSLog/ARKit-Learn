9ab17fe45c4d9867c9a0857700526053a5aaf684
wushiguang <wushiguang@jiuan.com>
2018-03-13 13:42:51 +0800

    获取单击节点事件
    获取单击节点事件

--- ARKit-Learn22/ARKit-Learn22/ViewController.m
+++ ARKit-Learn22/ARKit-Learn22/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <SceneKit/SceneKit.h>
 
 @interface ViewController ()
 
@@ -16,9 +17,156 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //创建游戏专用视图
+    SCNView *scnView = [[SCNView alloc]initWithFrame:self.view.bounds];
+    scnView.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:scnView];
+    
+    //创建游戏场景
+    SCNScene *scene = [SCNScene scene];
+    scnView.scene = scene;
+    
+    //创建照相机
+    SCNNode *cameraNode = [SCNNode node];
+    cameraNode.camera = [SCNCamera camera];
+    [scene.rootNode addChildNode:cameraNode];
+    
+    //创建一个环境光
+    SCNNode *lightNode = [SCNNode node];
+    lightNode.light = [SCNLight light];
+    lightNode.light.type = SCNLightTypeAmbient;
+    
+    //创建一个文字节点
+    SCNText *text = [SCNText textWithString:@"好好学习" extrusionDepth:1];
+    text.font = [UIFont systemFontOfSize:1];
+    SCNNode *textNode = [SCNNode nodeWithGeometry:text];
+    textNode.position = SCNVector3Make(-2, -0.5, -2);
+    [scene.rootNode addChildNode:textNode];
+    
+    
+    //创建三个事件
+    // 开始事件
+    SCNAnimationEvent *startEvt = [SCNAnimationEvent animationEventWithKeyTime:0 block:^(id<SCNAnimation>  _Nonnull animation, id  _Nonnull animatedObject, BOOL playingBackward) {
+        SCNNode *node = [SCNNode node];
+        node.geometry.firstMaterial.diffuse.contents = [UIColor redColor];
+    }];
+    //中间事件
+    SCNAnimationEvent *midEvt = [SCNAnimationEvent animationEventWithKeyTime:0.5 block:^(id<SCNAnimation>  _Nonnull animation, id  _Nonnull animatedObject, BOOL playingBackward) {
+        SCNNode *node = [SCNNode node];
+        node.geometry.firstMaterial.diffuse.contents = [UIColor purpleColor];
+        NSLog(@"==:%d",playingBackward);
+    }];
+    //结束事件
+    SCNAnimationEvent *endEvt = [SCNAnimationEvent animationEventWithKeyTime:1 block:^(id<SCNAnimation>  _Nonnull animation, id  _Nonnull animatedObject, BOOL playingBackward) {
+        SCNNode *node = [SCNNode node];
+        node.geometry.firstMaterial.diffuse.contents = [UIColor greenColor];
+    }];
+    
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position.z"];
+    animation.duration = 5;
+    animation.fromValue = @20;
+    animation.toValue = @-20;
+    animation.additive = YES;
+    animation.autoreverses = YES;
+    animation.animationEvents = @[startEvt,midEvt,endEvt];
+    animation.repeatCount = floorf(INT64_MAX);
+    [textNode addAnimation:animation forKey:nil];
 }
+/*
 
+ 25-SCNAnimationEvent
+ 
+ 官方解释
+ 用于在动画播放过程中的特定时间执行的闭包的容器(关键字特定时间\闭包)
+ 
+ 用法案例
+ 
+ 1.移动或者移除节点时,播放声音
+ 2.播放动画时,让隐藏的几何模型显示出来
+ 
+ 可能还有很多的使用案例需要我们去不断发现,SceneKit 提供给我们的选择有很多种,实际开发中,根据需求的不同再去做抉择。
+ 
+ 下面就举个简单的案例讲解一下用法
+ 第一步 创建工程(略)
+ 第二步 导入框架SceneKit
+ 第三步 创建游戏专用视图
+ let scnView = SCNView(frame: self.view.bounds, options: [SCNView.Option.preferredRenderingAPI.rawValue:true])
+ scnView.backgroundColor = UIColor.black
+ self.view.addSubview(scnView)
+ 
+ 第四步 创建游戏场景
+ let scene = SCNScene()
+ scnView.scene = scene
+ 
+ 第五步 创建照相机
+ let cameraNode = SCNNode()
+ cameraNode.camera = SCNCamera()
+ scene.rootNode.addChildNode(cameraNode)
+ 
+ 第六步 创建一个环境光
+ let ambientLightNode = SCNNode()
+ ambientLightNode.light = SCNLight()
+ ambientLightNode.light?.type = .ambient
+ scene.rootNode.addChildNode(ambientLightNode)
+ 
+ 第七步 创建一个文字节点
+ let text = SCNText(string: "酷走天涯", extrusionDepth: 1)
+ text.font = UIFont.systemFont(ofSize: 1)
+ let textNode = SCNNode(geometry: text)
+ textNode.position = SCNVector3Make(-2, -0.5, -2)
+ scene.rootNode.addChildNode(textNode)
+ 
+ 准备工作完成
+ 
+ 下面就是我们今天要将的内容了
+ 
+ 先来认识一下 我们今天最重要的一个初始化方法
+ 
+ public convenience init(keyTime time: CGFloat, block eventBlock: SceneKit.SCNAnimationEventBlock)
+ public typealias SCNAnimationEventBlock = (CAAnimation, Any, Bool)
+ 
+ 解释一下：
+ 
+ 1.time 这个参数你必须注意了,特别重要,它的取值范围[0-1] ,你可能要问为什么,这个时间参数是一个相对时间,不管你动画时间多长,这个动画时间需要传的参数,就只能是这个范围的值
+ 2.SCNAnimationEventBlock 回调事件的参数,你会发现没有说明,官方也没有具体给出说明,不过我们有调试工具,日志输出一下,就知道了,看文章的你幸运了我现在就告诉你参数是什么一下,第一个参数CAAnimation 类型,就是我们创建的动画,第二个参数any 当动画添加到节点上,那这个就是节点对象,第三个参数 动画是否回退执行
+ 
+ 重点内容来了,我们做一个颜色变化的事件,当动画开始执行是,我们的文字颜色为红色,动画指定一般颜色为紫色,动画执行完整时,颜色为绿色,我们重复这个行为
+ 
+ 创建三个事件
+ // 开始事件
+ let startEvt = SCNAnimationEvent(keyTime: 0) { (animation, animatedObject, playingBackward) in
+ let node = animatedObject as? SCNNode
+ node?.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+ 
+ 
+ }
+ // 中间事件
+ let midEvt = SCNAnimationEvent(keyTime: 0.5) { (animation, animatedObject, playingBackward) in
+ let node = animatedObject as? SCNNode
+ node?.geometry?.firstMaterial?.diffuse.contents = UIColor.purple
+ print(playingBackward)
+ 
+ }
+ 
+ // 结束事件
+ let endEvt = SCNAnimationEvent(keyTime: 1) { (animation, animatedObject, playingBackward) in
+ let node = animatedObject as? SCNNode
+ node?.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+ }
+ 
+ 创建一个动画对象把三个事件添加进去
+ let animation = CABasicAnimation(keyPath: "position.z")
+ animation.duration = 5
+ animation.fromValue = 0
+ animation.toValue = -20
+ animation.isAdditive = true
+ animation.autoreverses = true
+ animation.animationEvents = [startEvt,midEvt,endEvt]
+ animation.repeatCount = Float(Int64.max)
+ textNode.addAnimation(animation, forKey: nil)
+ 
+ 运行一下
+ */
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
--- ARKit-Learn23/ARKit-Learn23/ViewController.m
+++ ARKit-Learn23/ARKit-Learn23/ViewController.m
@@ -7,23 +7,122 @@
 //
 
 #import "ViewController.h"
+#import <SceneKit/SceneKit.h>
 
 @interface ViewController ()
-
+@property (nonatomic,weak) SCNView *scnView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    SCNView *scnView = [[SCNView alloc]initWithFrame:self.view.bounds];
+    scnView.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:scnView];
+    self.scnView = scnView;
+    
+    SCNScene *scene = [SCNScene scene];
+    scnView.scene = scene;
+    
+    SCNText *text = [SCNText textWithString:@"好好学习" extrusionDepth:1];
+    text.font = [UIFont systemFontOfSize:1];
+    SCNNode * textNode = [SCNNode nodeWithGeometry:text];
+    textNode.position = SCNVector3Make(-2, -0.5, -2);
+    [scnView.scene.rootNode addChildNode:textNode];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tap:)];
+    [scnView addGestureRecognizer:tap];
+    
+    
+    
+}
+- (void)tap:(UITapGestureRecognizer *)ges{
+    ;
+    NSArray *results = [self.scnView hitTest:[ges locationInView:self.scnView] options:nil];
+    SCNHitTestResult *hitResult = results.firstObject;
+    SCNNode *node = hitResult.node;
+    SCNVector3 vec = node.position;
+    
 }
 
+/*
+ 
+ 26-如何获取单击节点事件
+ 
+ ####本节学习目标
+ 
+ 掌握如何获取用户单机节点的时间
+ 
+ 先认识一个方法,这个方法在SCNView 里面
+ 
+ public func hitTest(_ point: CGPoint, options: [SCNHitTestOption : Any]? = nil) -> [SCNHitTestResult]
+ 
+ 当我们手点击屏幕时,要知道我们都点到了那些节点,我们应该怎么处理呢？
+ 
+ 首先我们添加一个手势到视图中去
+ 
+ let tap = UITapGestureRecognizer(target: self, action: #selector(tapHandle(gesture:)))
+ scnView.addGestureRecognizer(tap)
+ 
+ 然后，我们获取点击到的第一个节点
+ 
+ func tapHandle(gesture:UITapGestureRecognizer){
+ let results:[SCNHitTestResult] = (self.scnView?.hitTest(gesture.location(ofTouch: 0, in: self.scnView), options: nil))!
+ guard let firstNode  = results.first else{
+ return
+ }
+ // 点击到的节点
+ print(firstNode.node)
+ }
+ 
+ ####认识类SCNHitTestResult
+ 
+ open class SCNHitTestResult : NSObject {
+ 
+ /// 击中的几点
+ open var node: SCNNode { get }
+ 
+ /// 击中的几何体索引
+ open var geometryIndex: Int { get }
+ 
+ /// 击中的面的索引
+ open var faceIndex: Int { get }
+ 
+ /// 击中的本地坐标系统
+ open var localCoordinates: SCNVector3 { get }
+ 
+ 
+ /// 击中的世界坐标系统
+ open var worldCoordinates: SCNVector3 { get }
+ 
+ 
+ /// 击中节点的本地法线坐标
+ open var localNormal: SCNVector3 { get }
+ 
+ 
+ /// 击中的世界坐标系统的法线坐标
+ open var worldNormal: SCNVector3 { get }
+ 
+ 
+ / World transform of the node intersected. /
+open var modelTransform: SCNMatrix4 { get }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+/ The bone node hit. Only available if the node hit has a SCNSkinner attached. /
+@available(iOS 10.0, *)
+open var boneNode: SCNNode { get }
+
+
+/
+ @method textureCoordinatesWithMappingChannel:
+ @abstract Returns the texture coordinates at the point of intersection, for a given mapping channel.
+ @param channel The texture coordinates source index of the geometry to use. The channel must exists on the geometry otherwise {0,0} will be returned.
+ /
+open func textureCoordinates(withMappingChannel channel: Int) -> CGPoint
 }
+
+ */
 
 
 @end
