c18777c322d9b541cdd04c93f0d1e797a0ffc9d7
wushiguang <wushiguang@jiuan.com>
2018-03-13 14:04:48 +0800

    天空盒子
    天空盒子

--- ARKit-Learn24/ARKit-Learn24/ViewController.m
+++ ARKit-Learn24/ARKit-Learn24/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <SceneKit/SceneKit.h>
 
 @interface ViewController ()
 
@@ -16,14 +17,58 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //创建游戏专用视图
+    SCNView *scnView = [[SCNView alloc]initWithFrame:self.view.bounds options:nil];
+    scnView.backgroundColor = [UIColor lightGrayColor];
+    
+    [self.view addSubview:scnView];
+    scnView.allowsCameraControl = true;
+    
+    //创建游戏场景
+    SCNScene *scene = [SCNScene scene];
+    scnView.scene = scene;
+    
+    //创建天空盒子
+    scene.background.contents = @"skybox01_cube.png";
+    
+    //添加照相机
+    SCNNode *cameraNode = [SCNNode node];
+    cameraNode.camera = [SCNCamera camera];
+    
+    [scnView.scene.rootNode addChildNode:cameraNode];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+/*
 
+ 27-天空盒子
+ 
+ 代码实现过程
+ 第一步 找素材
+ 
+ 第二步 创建工程(略)
+ 第三步 导入框架SceneKit
+ 第四步 创建游戏专用视图
+ let scnView = SCNView(frame: self.view.bounds, options: [SCNView.Option.preferredRenderingAPI.rawValue:true])
+ scnView.backgroundColor = UIColor.black
+ self.view.addSubview(scnView)
+ scnView.allowsCameraControl = true
+ 
+ 第五步 创建游戏场景
+ let scene = SCNScene()
+ scnView.scene = scene
+ 
+ 第六步 创建天空盒子
+ scene.background.contents = "skybox01_cube.png"
+ 
+ 第七步 添加照相机(必须要的)
+ let cameraNode = SCNNode()
+ cameraNode.camera = SCNCamera()
+ scene.rootNode.addChildNode(cameraNode)
+ 
+ 运行效果:
+ 
+ */
 
 @end
